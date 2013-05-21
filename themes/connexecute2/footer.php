186287d5b20e4e8fc11a55cc05555c9793e4c3f4
Adam Johnson <ajohnson@bloomhealthco.com>
2013-05-21 14:35:11 -0500

    update

--- themes/connexecute2/footer.php
+++ themes/connexecute2/footer.php
@@ -1,23 +1,4 @@
 </div>
-<div class='owrapper bg_dark' id='o_footer'>
-        <div class='iwrapper'>
-                <div id='footer_column_1'>
-                <?php
-                        $data = get_post_by_tag('site_content_about_us');
-                        echo '<h1>'.$data['title'].'</h1>';
-                        echo '<p>'.$data['content'].'</p>';
-                ?>
-                <p>&copy; <?php echo date('Y'); ?>, Hurley Law Group
-		</div>
-                <div id='footer_column_2'>
-                <?php
-                        $data = get_post_by_tag('site_content_contact_us');
-                        echo '<h1>'.$data['title'].'</h1>';
-                        echo '<p>'.$data['content'].'</p>';
-                ?>		
-		</div>
-        </div>
-</div>
 <?php wp_footer(); ?>
 
 </body>
--- themes/connexecute2/header.php
+++ themes/connexecute2/header.php
@@ -4,7 +4,6 @@
 <title><?php wp_title(); ?></title>
 <meta charset="UTF-8" />
 <link rel="stylesheet" type="text/css" media="all" href="/theme/style.css" />
-<link href='http://fonts.googleapis.com/css?family=Molle:400italic|Italiana|Amatic+SC|Happy+Monkey|Racing+Sans+One|Advent+Pro:400,700' rel='stylesheet' type='text/css'>
 <link rel="shortcut icon" href="/theme/favicon.ico"/>
 
 <!-- include jQuery library -->
@@ -19,12 +18,8 @@
 <?php wp_head(); ?>
 </head>
 
-<body class='bg_dark'>
-<div style="font-family: 'Noticia Text', serif; font-weight: normal;"></div>
-<div class='owrapper bg_light'>
-	<div class='iwrapper' id='header'>
-		<div class='logo_hurleylawgroup' id='header_logo'></div>
-		<div class='navigation navigation_horizontal' id='header_navigation'>
+<body>
+	<div id='owrapper'>
+		<div id='iwrapper'>
 			<?php wp_nav_menu( array( 'menu' => 'header-menu', 'sort_column' => 'menu_order' ) ); ?>
 		</div>
-	</div>
