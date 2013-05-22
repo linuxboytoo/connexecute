26db22f32ea520703226a4441b580f6ce1aae6b3
Adam Johnson <ajohnson@bloomhealthco.com>
2013-05-21 23:09:58 -0500

    update

--- themes/connexecute2/main_banners/homepage.php
+++ themes/connexecute2/main_banners/homepage.php
@@ -1,1 +1,2 @@
-TEST
+<div style='border: 1px solid #000000; width: 498px; height: 273px;'>
+</div>
--- themes/connexecute2/template-banner.php
+++ themes/connexecute2/template-banner.php
@@ -1,8 +1,8 @@
 <div class='owrapper'>
 	<div class='iwrapper'>
-		<?php if(file_exists(TEMPLATEPATH.'/main_banners/homepage1.php')) { ?>
+		<?php if(file_exists(TEMPLATEPATH.'/main_banners/homepage.php')) { ?>
 			<div id='main_banner'>
-				<?php @include(TEMPLATEPATH.'/main_banners/homepage1.php'); ?>
+				<?php @include(TEMPLATEPATH.'/main_banners/homepage.php'); ?>
 			</div>
 		<?php } ?>
 	</div>
