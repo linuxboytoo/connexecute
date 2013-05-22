4e8b1fdbc89e0fbdd7f18fa39e0688abaa309f3f
Adam Johnson <ajohnson@bloomhealthco.com>
2013-05-21 22:58:31 -0500

    update

--- themes/connexecute2/template-banner.php
+++ themes/connexecute2/template-banner.php
@@ -1,7 +1,7 @@
 <div class='owrapper'>
 	<div class='iwrapper'>
 		<div id='main_banner'>
-			<?php echo get_blog_info('stylesheet_directory'); ?>
+			<?php echo get_bloginfo('stylesheet_directory'); ?>
 		</div>
 	</div>
 </div>
