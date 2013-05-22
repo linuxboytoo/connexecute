88feaa87fbaa4a8790730161cfde1e8bb28195ef
Adam Johnson <ajohnson@bloomhealthco.com>
2013-05-22 15:49:40 -0500

    Update

--- themes/connexecute2/js/custom.js
+++ themes/connexecute2/js/custom.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
 	
 	jQuery('input[default]')
-		.focus(function() { if ($(this).val()==$(this).attr("default")) { $(this).val(""); $(this).css('color','#000000'); } })
-		.blur( function() { if ($(this).val()=="") { $(this).val($(this).attr("default")); $(this).css('color','#ff0000'); } });
+		.focus(function() { if ($(this).val()==$(this).attr("default")) { $(this).val(""); $(this).addClass('empty'); } })
+		.blur( function() { if ($(this).val()=="") { $(this).val($(this).attr("default")); $(this).removeClass('empty'); } });
 
 });
