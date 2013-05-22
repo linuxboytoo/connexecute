98a5fa97aa5158854faad968ee9193e040cadb68
Adam Johnson <ajohnson@bloomhealthco.com>
2013-05-22 09:03:42 -0500

    update

--- themes/connexecute2/main_banners/homepage.php
+++ themes/connexecute2/main_banners/homepage.php
@@ -1,7 +1,6 @@
 <div style='float: left; border: 1px solid #555555; width: 498px; height: 273px;'>
 </div>
 <div style='float: left; width: 59px;'>&nbsp;</div>
-<div style='float: left; width: 401px;'>
-<div style='background-color: #7ac143; background-image: url("/theme/images/horizontal-dots.png"); width: 400px; height: 3px;'></div>
-TESTNiG
+<div style='float: left; width: 401px; position: relative;'>
+	<div class='dottedhr bg_green' style='position: absolute; top: 64px; left: 0px;'></div>
 </div>
