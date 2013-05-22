5c1890dc544dce95a79a357e235f414a7aee2058
Adam Johnson <ajohnson@bloomhealthco.com>
2013-05-22 17:44:27 -0500

    Update

--- themes/connexecute2/section-category_twocolumn.php
+++ themes/connexecute2/section-category_twocolumn.php
@@ -1,2 +1,29 @@
-<?php
-echo "TWO COLUMN";
+<div class='owrapper'>
+	<div class='iwrapper'>
+		<?php		
+				global $wp_query;
+				global $post;
+ 
+				$acs = new Acs();
+				$posts = $acs->get_post_ids('Services');
+ 
+				$args = array(
+					'post__in' 		 => $posts,
+					'orderby'		 => 'post__in',
+				);
+
+ 				$wp_query = new WP_Query( $args );
+ 
+			 	if( have_posts() )
+ 				{
+ 					while( have_posts())
+ 					{
+ 						the_post();
+ 		?>
+ 						<h1><?php the_title(); ?></h1>
+ 		<?php
+ 					}
+ 				}
+ 		?>
+	</div>
+</div>
