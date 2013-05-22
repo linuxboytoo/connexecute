473fb8fb62dbac026b1ffaf15eaeff09c9fccc68
Adam Johnson <ajohnson@bloomhealthco.com>
2013-05-22 12:55:11 -0500

    Update

--- themes/connexecute2/main_banners/homepage.php
+++ themes/connexecute2/main_banners/homepage.php
@@ -2,8 +2,15 @@
 </div>
 <div style='float: left; width: 59px;'>&nbsp;</div>
 <div style='float: left; width: 401px; position: relative;'>
+	<!-- BLUE SIDE BAR -->
 	<div class='bg_darkblue' style='position: absolute; top: 0; right: 0; width: 61px; height: 275px;'></div> 
+	<!-- GREEN DOTTED LINE -->
 	<div class='dottedhr-green' style='position: absolute; top: 64px; left: 0px; width: 100%'></div>
+	<!-- DOWNLOAD YOUR FREE -->
 	<div class='fg_lightblue' style='font-family: Allura; font-size: 40px; position: absolute; top: 17px; left: 15px;'>Download &nbsp;Your Free</div>
+	<!-- ARROW -->
 	<div style='position: absolute; background-image: url("/theme/images/arrow-drawndown.png"); width: 49px; height: 60px; top: 31px; right: 35px; z-index: 200;'></div>
+	<div style='position: absolute; top: 97px; left: 21px; width: 195px; height: 178px;'>
+	test<br />test</br />test
+	</div> 
 </div>
