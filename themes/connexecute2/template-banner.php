f5d44a1057531c1708f73a2d6ebf3318ef3e64d5
Adam Johnson <ajohnson@bloomhealthco.com>
2013-05-21 23:02:00 -0500

    update

--- themes/connexecute2/template-banner.php
+++ themes/connexecute2/template-banner.php
@@ -1,7 +1,7 @@
 <div class='owrapper'>
 	<div class='iwrapper'>
 		<div id='main_banner'>
-			<?php echo TEMPLATEPATH; ?>
+			<?php @include(TEMPLATEPATH.'main_banners/homepage.php'); ?>
 		</div>
 	</div>
 </div>
