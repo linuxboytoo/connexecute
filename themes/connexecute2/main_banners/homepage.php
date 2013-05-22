71a1ffe2e4cd93d48c3a3277159714cce20893fe
Adam Johnson <ajohnson@bloomhealthco.com>
2013-05-22 09:04:13 -0500

    update

--- themes/connexecute2/main_banners/homepage.php
+++ themes/connexecute2/main_banners/homepage.php
@@ -2,5 +2,6 @@
 </div>
 <div style='float: left; width: 59px;'>&nbsp;</div>
 <div style='float: left; width: 401px; position: relative;'>
+test
 	<div class='dottedhr bg_green' style='position: absolute; top: 64px; left: 0px;'></div>
 </div>
