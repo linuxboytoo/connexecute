a6a001bcb8438c78a1f2f9c7c988fa5fd9428209
Adam Johnson <ajohnson@bloomhealthco.com>
2013-05-22 12:45:19 -0500

    Update

--- themes/connexecute2/main_banners/homepage.php
+++ themes/connexecute2/main_banners/homepage.php
@@ -5,4 +5,5 @@
 	<div class='bg_darkblue' style='position: absolute; top: 0; right: 0; width: 61px; height: 275px;'></div> 
 	<div class='dottedhr-green' style='position: absolute; top: 64px; left: 0px; width: 100%'></div>
 	<div class='fg_lightblue' style='font-family: Allura; font-size: 40px; position: absolute; top: 17px; left: 15px;'>Download &nbsp;Your Free</div>
+	<div style='background-image: url("/theme/images/arrow-drawndown.png"); width: 49px; height: 60px;'></div>
 </div>
