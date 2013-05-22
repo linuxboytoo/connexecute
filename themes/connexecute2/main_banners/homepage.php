b7bdb5017be999f92ce69d834c39acc71194fcf3
Adam Johnson <ajohnson@bloomhealthco.com>
2013-05-22 08:45:25 -0500

    update

--- themes/connexecute2/main_banners/homepage.php
+++ themes/connexecute2/main_banners/homepage.php
@@ -1,6 +1,6 @@
 <div style='float: left; border: 1px solid #555555; width: 498px; height: 273px;'>
 </div>
-<div style='float: left; width: 59px;'></div>
+<div style='float: left; width: 59px;'>&nbsp;</div>
 <div style='float: left; width: 401px; background-color: #ff0000;'>
 TESTNG
 </div>
