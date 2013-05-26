d0de148c87890d6924167619a63c9565498c33c6
Adam Johnson <ajohnson@bloomhealthco.com>
2013-05-26 14:04:24 -0500

    Further Commits

---
+++ themes/connexecute2/category.php
@@ -0,0 +1,58 @@
+<?php
+	$temp = get_the_category(); 
+	$rootcat_name = $temp[sizeof($temp)-1]->cat_name;
+
+	global $wp_query;
+	global $post;
+			
+	$startargs = $wp_query->query_vars;
+
+
+	/* Special case to use plugin "Advanced Custom Sort". ACS Group will be used if title of group matches category title */
+	if(class_exists('Acs'))
+	{	
+ 		$acs = new Acs();
+		$acsposts = $acs->get_post_ids($rootcat_name);
+    
+ 		if($acsposts[0])
+		{ 	
+			$newargs = array(
+				'post__in' 		 => $acsposts,
+				'orderby'		 => 'post__in',
+			);
+			$args = array_merge($startargs,$newargs);
+			$wp_query = new WP_Query( $args );
+		}
+	}
+?>
+
+
+<?php get_header(); ?>
+		<div class=owrapper' style='height: 25px;'></div>
+		
+		<div class='owrapper'>
+			<div class='iwrapper'>
+					<a name="top"></a>
+					<h1><?php echo $rootcat_name; ?></h1>
+					<?php if( have_posts() ) { while( have_posts() ) { the_post(); $link = get_permalink(); ?>
+					<div id='main_content'>
+						<div class='image'>
+							<?php 	
+								if ( has_post_thumbnail() ) { the_post_thumbnail(Array(120,120)); } 
+								else { echo "<img src='/wp-content/themes/connexecute/images/logo.png' width=170 height=170>"; } 
+							?>
+						</div>
+						<div class='content'>
+							<h2><?php echo (!empty($link)?'<a href="'.$link.'">':'').get_the_title().(!empty($link)?'</a>':''); ?></h2>
+							<?php the_content(); ?>
+						</div>
+											<div style='clear: both;'></div>
+
+					</div>
+					<div style='clear: both;'></div>
+					<?php } } ?>
+					<a href='#top' style='padding: 25px; display: block;'>return to top</a>
+			</div>
+		</div>
+
+<?php get_footer(); ?>
--- themes/connexecute2/index.php
+++ themes/connexecute2/index.php
@@ -1,15 +1,14 @@
-<?php 
-		get_header();
-		get_template_part('section','banner');
+<?php 	
+		get_header(); 
 ?>
-		<div class='iwrapper'></div>
+		<div class=owrapper' style='height: 25px;'></div>
 		
 		<div class='owrapper'>
 			<div class='iwrapper'>
 					<a name="top"></a>
-					<?php if( have_posts() ) { while( have_posts() ) { the_post(); ?>
+					<?php if( have_posts() ) { while( have_posts() ) { the_post(); $link = get_permalink(); ?>
 					<div id='main_content'>
-						<h2><?php the_title(); ?></h2>
+						<h2><?php echo (!empty($link)?'<a href="'.$link.'">':'').get_the_title().(!empty($link)?'</a>':''); ?></h2>
 						<div class='content'>
 							<?php the_content(); ?>
 						</div>
@@ -17,22 +16,5 @@
 					<?php } } ?>
 			</div>
 		</div>
-		
-		<div class='iwrapper'>
-		<?php get_template_part('section','category_twocolumn'); ?>
-		</div>
-		<div class='owrapper bg_green'>
-			<div id="lower_banner" class='iwrapper bg_green'>
-				<h1 style='padding-left: 5px;' class='fancyfont'>Get the Free Equip Update</h1>
-				<h2 style='padding-left: 100px;'>useful <a href="#">tips</a> to grow your business</h2>
-			</div>
-		</div>
-		<div class='owrapper'>
-			<div class='iwrapper' style='height: 50px;'>
-			test
-			</div>
-		</div>
 
-<?php
-		get_footer(); 
-?>
+<?php get_footer(); ?>
